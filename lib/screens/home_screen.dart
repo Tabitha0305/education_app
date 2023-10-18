fae23f32e46c985eb0ad566434be405644ee61ff
docas <ajibolatabita@gmail.com>
2023-10-18 16:22:39 +0100

    education app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,162 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  // creating static data in lists
+
+  List catNames = [
+    "Category",
+    'Classes',
+    'Free Course',
+    'BookStore'
+    'Live Course',
+    'LeaderBoard',
+  ];
+
+  List<Color> catColors = [
+    Color(0xFFFFCF2F),
+    Color(0xFF6FE08D),
+    Color(0xFF61BDFDF),
+    Color(0xFFFC7F7F),
+    Color(0xFFCB84FB),
+    Color(0xFF78E667)
+  ];
+  List<Icon> catIcons = [
+    Icon(Icons.category, color:Colors.white, size:30),
+    Icon(Icons.video_library, color:Colors.white, size:30),
+    Icon(Icons.assignment, color:Colors.white, size:30),
+    Icon(Icons.store, color:Colors.white, size:30),
+    Icon(Icons.play_circle_fill, color:Colors.white, size:30),
+    Icon(Icons.emoji_events, color:Colors.white, size:30),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-// Scaffold
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+              color: Color(0xFF674AEF),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ), 
+            ), 
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                       size: 30,          
+                       color: Colors.white,
+                       ), // Icon
+                       Icon(
+                         Icons.notifications,
+                         size: 30,
+                         color: Colors.white,
+                       ), // Icon
+                      ],
+                     ), // Row
+                     SizedBox(height: 20),
+                     Padding(padding: EdgeInsets.only(left: 3, bottom: 15)
+                     child: Text(
+                      "Hi programmer",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                        color: Colors.white,
+                      ), // TextStyle
+                     ), // Text
+                     ), // padding
+                     Container(
+                      margin: EdgeInsets.only(top: 5,bottom: 20),
+                      width: MediaQuery.of(context).size.width,
+                      height: 55,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+
+                      ), 
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search here....",
+                        hintStyle: TextStyle(
+                          color:Colors.black.withOpacity(0.5),
+                        ), 
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 25,
+                        ), 
+                        ), 
+                      ), 
+                     ), 
+              ],
+            ), 
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15,right: 15),
+            child: Column(
+              children: [
+              GridView.builder(
+                itemCount: catNames.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.1,
+                ), // SliverGridDelegateWithFixedCrossAxisCount
+                 itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: catColors[index],
+                          shape: BoxShape.circle,
+                        ), 
+                        child: Center(
+                          child: catIcons[index],
+                          ),
+                      ), 
+                      SizedBox(height: 10),
+                      Text(
+                        catNames[index],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ), 
+                    ],
+                  );
+
+                 },
+            ),
+         ] ),), 
+   ] )); 
+
   }
 }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+                
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -23,15 +24,93 @@ class WelcomeScreen extends StatelessWidget {
                   height: MediaQuery.of(context).size.height / 1.6,
                   decoration: BoxDecoration(
                     color: Color(0xFF674AEF),
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular((70)),
-                    ),
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(70)),
                   ),
                   child: Center(
-                    child: Image.asset("assets/images/books.png"),
+                    child: Image.asset(
+                      "images/books.png",
+                      scale: 0.8,
+                    ),
                   ),
                 )
               ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                decoration: BoxDecoration(
+                  color: Color(0xFF674AEF),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                padding: EdgeInsets.only(top: 40, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Learning is Everything",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        "Learning with Miss D, wherever you are.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black.withOpacity(0.6),
+                        ), 
+                      ), 
+                    ),
+                    SizedBox(height: 60),
+                    Material(
+                      color: Color(0xFF674AEF),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap:() {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(),
+                          )); 
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 80),
+                        ), // EdgeInsets.symmetrical
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ), 
+                        ), 
+                      ), 
+                    ),     
+                  ]
+                ),
+              ),
             ),
           ],
         ),
